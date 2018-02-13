7aee8abdb04ff1911bbcf6c19f2bee97f6037ead
javpet <javorkai.peter@gmail.com>
2018-02-13 19:42:16 +0100

    SEO friendly URL added

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,7 @@ class Review < ApplicationRecord
   validates :body, length: { minimum: 10 }
   validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
+  def to_param
+    id.to_s + '-' + title.parameterize # We put the title in the URL, to be SEO friendly
+  end
 end
