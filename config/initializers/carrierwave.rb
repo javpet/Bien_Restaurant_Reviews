508ddcbd90d7f037c8d8655935f45c194d75d607
javpet <javorkai.peter@gmail.com>
2018-03-03 20:44:17 +0100

    Added encryption

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     '***',       # required
-    aws_secret_access_key: '***',                        # required
+    aws_access_key_id:     Rails.application.secrets.aws_key,       # required
+    aws_secret_access_key: Rails.application.secrets.aws_secret                        # required
   }
   config.fog_directory  = 'bien-reviews-superhi'                                   # required
 
